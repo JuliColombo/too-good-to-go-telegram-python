bd43a39f1971663f6cd5c559d2ec6d7e6d41af09
Julieta Colombo <colombo.julieta.natalia@gmail.com>
2023-03-11 16:10:49 +0100

    Run get-credentials

--- netlify/functions/run-tgtg.js
+++ netlify/functions/run-tgtg.js
@@ -1,6 +1,11 @@
 exports.handler = async function (event, context) {
     console.log("Received event:", event);
-
+    $.ajax({
+        type: "POST",
+        url: "~/get-credentials.py",
+    }).done(function (o) {
+        console.log(o)
+    });
     $.ajax({
         type: "POST",
         url: "~/main.py",
